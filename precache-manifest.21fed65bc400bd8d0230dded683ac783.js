a0578fc7cc99d9720ebdcf602ba5823fad008507
Tim <timji0210@gmail.com>
2020-05-07 22:28:36 +0800

    Updates

---
+++ precache-manifest.21fed65bc400bd8d0230dded683ac783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe71212fd8afd44bab14f0cdd5cfd901",
+    "url": "/wildlings/index.html"
+  },
+  {
+    "revision": "6891119359daf5ef3950",
+    "url": "/wildlings/static/css/2.chunk.css"
+  },
+  {
+    "revision": "6ac07c7f59795b3731b7",
+    "url": "/wildlings/static/css/main.chunk.css"
+  },
+  {
+    "revision": "6891119359daf5ef3950",
+    "url": "/wildlings/static/js/2.wildlings.chunk.js"
+  },
+  {
+    "revision": "6ac07c7f59795b3731b7",
+    "url": "/wildlings/static/js/main.wildlings.chunk.js"
+  },
+  {
+    "revision": "7f43d437843b889c3469",
+    "url": "/wildlings/static/js/wildlings.bundle.js"
+  }
+]);
